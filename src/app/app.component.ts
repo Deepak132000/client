1a3cc1bdd642dc9d26fd4a34e8966d95702b04ed
Deepak jain <deepakyobro915@gmail.com>
2024-09-27 10:33:09 +0530

    fourth commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,26 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, OnInit, inject } from '@angular/core';
+import { CommonModule, NgFor } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  
+  http = inject(HttpClient);
   title = 'client';
+  users:any;
+
+  ngOnInit(): void {
+    this.http.get("http://localhost:5294/api/users").subscribe({
+      next: response=> this.users = response,
+      error: error => console.error(error),
+      complete: () => console.log('complete'),
+    });
+  }
 }
